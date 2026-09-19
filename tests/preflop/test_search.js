86a168b76b93d4e4905aa37853f63ec9f656af91
aradenac <abel.radenac@gmail.com>
2026-09-19 16:50:42 +0200

    [D][#353] Prove alternative contract before inherited posterior gate

--- tests/preflop/test_search.js
+++ tests/preflop/test_search.js
@@ -5,8 +5,8 @@ const assert=require('node:assert/strict');
 const Contract=require('../../src/preflop/contract.js');
 const Decision=require('../../src/preflop/decision.js');
 const Search=require('../../src/preflop/search.js');
-require('./test_iso_sizing_diagnostics.js');
 require('./test_hero_preflop_alternatives.js');
+require('./test_iso_sizing_diagnostics.js');
 
 const POS=['LJ','HJ','CO','BTN','SB','BB'];
 const stacks=Object.fromEntries(POS.map(p=>[p,100]));
